c5425176d2153b2e7e25920d6a9487d538b5d1f2
Dieter Peirs <github@dieterpeirs.com>
2020-12-07 18:42:28 +0100

    design home page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,103 +10,103 @@ window.addEventListener('load', function() {
     navigator.serviceWorker.register('/service-worker.js');
   }
 
-  // THEME SWITCH
-  const STORAGE_KEY = 'user-color-scheme';
-  const COLOR_MODE_KEY = '--color-mode';
-  const modeToggleButton = document.querySelector('.js-mode-toggle');
-  const modeToggleText = document.querySelector('.js-mode-toggle-text');
-  const modeStatusElement = document.querySelector('.js-mode-status');
+  // // THEME SWITCH
+  // const STORAGE_KEY = 'user-color-scheme';
+  // const COLOR_MODE_KEY = '--color-mode';
+  // const modeToggleButton = document.querySelector('.js-mode-toggle');
+  // const modeToggleText = document.querySelector('.js-mode-toggle-text');
+  // const modeStatusElement = document.querySelector('.js-mode-status');
 
-  /**
-   * Pass in a custom prop key and this function will return its
-   * computed value.
-   * A reduced version of this: https://andy-bell.design/wrote/get-css-custom-property-value-with-javascript/
-   */
-  const getCSSCustomProp = (propKey) => {
-    let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
+  // /**
+  //  * Pass in a custom prop key and this function will return its
+  //  * computed value.
+  //  * A reduced version of this: https://andy-bell.design/wrote/get-css-custom-property-value-with-javascript/
+  //  */
+  // const getCSSCustomProp = (propKey) => {
+  //   let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
 
-    // Tidy up the string if there’s something to work with
-    if (response.length) {
-      response = response.replace(/\'|"/g, '').trim();
-    }
+  //   // Tidy up the string if there’s something to work with
+  //   if (response.length) {
+  //     response = response.replace(/\'|"/g, '').trim();
+  //   }
 
-    // Return the string response by default
-    return response;
-  };
+  //   // Return the string response by default
+  //   return response;
+  // };
 
-  /**
-   * Takes either a passed settings ('light'|'dark') or grabs that from localStorage.
-   * If it can’t find the setting in either, it tries to load the CSS color mode,
-   * controlled by the media query
-   */
-  const applySetting = passedSetting => {
-    let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
+  // /**
+  //  * Takes either a passed settings ('light'|'dark') or grabs that from localStorage.
+  //  * If it can’t find the setting in either, it tries to load the CSS color mode,
+  //  * controlled by the media query
+  //  */
+  // const applySetting = passedSetting => {
+  //   let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
 
-    if(currentSetting) {
-      document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
-      setButtonLabelAndStatus(currentSetting);
-    }
-    else {
-      setButtonLabelAndStatus(getCSSCustomProp(COLOR_MODE_KEY));
-    }
-  }
+  //   if(currentSetting) {
+  //     document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
+  //     setButtonLabelAndStatus(currentSetting);
+  //   }
+  //   else {
+  //     setButtonLabelAndStatus(getCSSCustomProp(COLOR_MODE_KEY));
+  //   }
+  // }
 
-  /**
-   * Get’s the current setting > reverses it > stores it
-   */
-  const toggleSetting = () => {
-    let currentSetting = localStorage.getItem(STORAGE_KEY);
+  // /**
+  //  * Get’s the current setting > reverses it > stores it
+  //  */
+  // const toggleSetting = () => {
+  //   let currentSetting = localStorage.getItem(STORAGE_KEY);
 
-    switch(currentSetting) {
-      case null:
-        currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
-        break;
-      case 'light':
-        currentSetting = 'dark';
-        break;
-      case 'dark':
-        currentSetting = 'light';
-        break;
-    }
+  //   switch(currentSetting) {
+  //     case null:
+  //       currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
+  //       break;
+  //     case 'light':
+  //       currentSetting = 'dark';
+  //       break;
+  //     case 'dark':
+  //       currentSetting = 'light';
+  //       break;
+  //   }
 
 
-    localStorage.setItem(STORAGE_KEY, currentSetting);
+  //   localStorage.setItem(STORAGE_KEY, currentSetting);
 
-    return currentSetting;
-  }
+  //   return currentSetting;
+  // }
 
-  /**
-   * A shared method for setting the button text label and visually hidden status element
-   */
-  const setButtonLabelAndStatus = currentSetting => {
-    modeToggleText.innerText = `${currentSetting === 'dark' ? 'Light' : 'Dark'} mode`;
-    modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
-  }
+  // /**
+  //  * A shared method for setting the button text label and visually hidden status element
+  //  */
+  // const setButtonLabelAndStatus = currentSetting => {
+  //   modeToggleText.innerText = `${currentSetting === 'dark' ? 'Light' : 'Dark'} mode`;
+  //   modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
+  // }
 
-  /**
-   * Clicking the button runs the apply setting method which grabs its parameter
-   * from the toggle setting method.
-   */
-  modeToggleButton.addEventListener('click', evt => {
-    evt.preventDefault();
+  // /**
+  //  * Clicking the button runs the apply setting method which grabs its parameter
+  //  * from the toggle setting method.
+  //  */
+  // modeToggleButton.addEventListener('click', evt => {
+  //   evt.preventDefault();
 
-    applySetting(toggleSetting());
-  });
+  //   applySetting(toggleSetting());
+  // });
 
-  applySetting();
+  // applySetting();
 
-  // RETAIN SCROLL HISTORY
-  if ('scrollRestoration' in history) {
-    history.scrollRestoration = 'manual';
-  }
+  // // RETAIN SCROLL HISTORY
+  // if ('scrollRestoration' in history) {
+  //   history.scrollRestoration = 'manual';
+  // }
 
-  // add lazyload attribute to single post images
-  const singlePostImgs = document.querySelectorAll('.single-post .post img');
-  if (singlePostImgs) {
-    for (var i = 0; i < singlePostImgs.length; i++) {
-      singlePostImgs[i].setAttribute('loading', 'lazy');
-    }
-  }
+  // // add lazyload attribute to single post images
+  // const singlePostImgs = document.querySelectorAll('.single-post .post img');
+  // if (singlePostImgs) {
+  //   for (var i = 0; i < singlePostImgs.length; i++) {
+  //     singlePostImgs[i].setAttribute('loading', 'lazy');
+  //   }
+  // }
 
   /* ======================
    #MOBILE NAVIGATION
--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -4,16 +4,16 @@ module.exports = {
       <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-${name}"></use>
     </svg>`
   },
-  image: function (image, altText, modifier) {
+  image: function (format, image, altText, modifier) {
     return `<img
     class="c-image ${modifier} lazyload fade-in"
-    src="/assets/images/png/${image}@thumb.png"
+    src="/assets/images/${format}/${image}@thumb.${format}"
     data-sizes="auto"
     data-srcset="
-      /assets/images/png/${image}@600.png 300w,
-      /assets/images/png/${image}@900.png 600w,
-      /assets/images/png/${image}@1280.png 900w,
-      /assets/images/png/${image}@1600.png 1280w"
+      /assets/images/${format}/${image}@600.${format} 300w,
+      /assets/images/${format}/${image}@900.${format} 600w,
+      /assets/images/${format}/${image}@1280.${format} 900w,
+      /assets/images/${format}/${image}@1600.${format} 1280w"
     alt="${altText}">`
   }
 }
