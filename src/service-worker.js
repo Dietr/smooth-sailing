f5fe10b2fa6e314ec3af2b4dbfc1adb7758f8cef
Dieter Peirs <github@dieterpeirs.com>
2020-12-10 13:32:44 +0100

    update generic outline

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,8 +4,8 @@
 // Reference and attribution: https://github.com/andybelldesign/hylia/blob/master/src/_includes/partials/global/service-worker.js
 
 const CACHE_KEYS = {
-  PRE_CACHE: `precache-6`,
-  RUNTIME: `runtime-6`
+  PRE_CACHE: `precache-7`,
+  RUNTIME: `runtime-7`
 };
 
 // URLS that we don’t want to end up in the cache
