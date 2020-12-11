6ef4a5a25f4ebfe5607db93cc74a796658a6576c
Dieter Peirs <github@dieterpeirs.com>
2020-12-11 14:13:28 +0100

    fix e-mail address

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,8 +4,8 @@
 // Reference and attribution: https://github.com/andybelldesign/hylia/blob/master/src/_includes/partials/global/service-worker.js
 
 const CACHE_KEYS = {
-  PRE_CACHE: `precache-202012111134`,
-  RUNTIME: `runtime-202012111134`
+  PRE_CACHE: `precache-202012111412`,
+  RUNTIME: `runtime-202012111412`
 };
 
 // URLS that we don’t want to end up in the cache
